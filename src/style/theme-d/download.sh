55657cf743e37b6d23f742a310ef55c5610d64bf
Robbert Broersma <mail@robbertbroersma.nl>
2021-07-06 09:08:50 +0200

    feat: two new themes

---
+++ src/style/theme-d/download.sh
@@ -0,0 +1,17 @@
+#!/bin/bash -e
+
+curl --remote-name https://www.denhaag.nl/static/denhaagrestylepresentation/fonts/TheMix_LP_500_.eot
+curl --remote-name https://www.denhaag.nl/static/denhaagrestylepresentation/fonts/TheMix_LP_500_.eot
+curl --remote-name https://www.denhaag.nl/static/denhaagrestylepresentation/fonts/TheMix_LP_500_.woff
+curl --remote-name https://www.denhaag.nl/static/denhaagrestylepresentation/fonts/TheMix_LP_700_.eot
+curl --remote-name https://www.denhaag.nl/static/denhaagrestylepresentation/fonts/TheMix_LP_700_.eot
+curl --remote-name https://www.denhaag.nl/static/denhaagrestylepresentation/fonts/TheMix_LP_700_.woff
+curl --remote-name https://www.denhaag.nl/static/denhaagrestylepresentation/fonts/TheSans_LP_500_Plain.eot
+curl --remote-name https://www.denhaag.nl/static/denhaagrestylepresentation/fonts/TheSans_LP_500_Plain.eot
+curl --remote-name https://www.denhaag.nl/static/denhaagrestylepresentation/fonts/TheSans_LP_500_Plain.woff
+curl --remote-name https://www.denhaag.nl/static/denhaagrestylepresentation/fonts/TheSans_LP_600_SemiBold.eot
+curl --remote-name https://www.denhaag.nl/static/denhaagrestylepresentation/fonts/TheSans_LP_600_SemiBold.eot
+curl --remote-name https://www.denhaag.nl/static/denhaagrestylepresentation/fonts/TheSans_LP_600_SemiBold.woff
+curl --remote-name https://www.denhaag.nl/static/denhaagrestylepresentation/fonts/TheSans_LP_700_Bold.eot
+curl --remote-name https://www.denhaag.nl/static/denhaagrestylepresentation/fonts/TheSans_LP_700_Bold.eot
+curl --remote-name https://www.denhaag.nl/static/denhaagrestylepresentation/fonts/TheSans_LP_700_Bold.woff
